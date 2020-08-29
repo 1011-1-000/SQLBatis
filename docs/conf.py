29758f890def128e58ca83fa75842ccd99987912
10111000 <273775401@qq.com>
2020-08-29 22:58:26 +0800

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,20 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 
+def run_apidoc(_):
+    from sphinx.apidoc import main
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = os.path.join(cur_dir, "..", "sqlbatis")
+    main(['-e', '-o', cur_dir, module, '--force'])
+
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
+
+
 # -- Project information -----------------------------------------------------
 
 project = 'SQLBatis'
