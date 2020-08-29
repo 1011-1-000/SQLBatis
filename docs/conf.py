c32a0430835ff599ac7bf734b644c8c6d8cf4fc9
10111000 <273775401@qq.com>
2020-08-30 00:19:58 +0800

    conf in docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,22 +16,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_imports = ['sqlalchemy', 'MarkupSafe',
-                   'prettytable', 'werkzeug', 'Click', 'alembic']
-
-# def run_apidoc(_):
-#     from sphinx.apidoc import main
-#     import os
-#     import sys
-#     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-#     cur_dir = os.path.abspath(os.path.dirname(__file__))
-#     module = os.path.join(cur_dir, "..", "sqlbatis")
-#     main(['-e', '-o', cur_dir, module, '--force'])
-
-
-# def setup(app):
-#     app.connect('builder-inited', run_apidoc)
-
 
 # -- Project information -----------------------------------------------------
 
